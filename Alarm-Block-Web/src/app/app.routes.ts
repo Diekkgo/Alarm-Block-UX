bc3ecd65920ba18ff0cec75c10e3820075eda8f9
Diekkgo <da.gomezs12@uniandes.edu.co>
2026-09-17 17:19:46 -0500

    Fear: Screen 2 - Dashboard

--- Alarm-Block-Web/src/app/app.routes.ts
+++ Alarm-Block-Web/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
+import { Dashboard } from './components/dashboard/dashboard';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: Login
+    },
+    {
+        path: 'dashboard',
+        component: Dashboard
     },
     {
         path: '**',
