55a2bf7e1d99fa224bb5763358fd149710146e0d
Carlos Zerpa <c.zerpa@uniandes.edu.co>
2026-09-18 19:39:22 -0500

    Feat: Screen 4 - Preferences

---
+++ Alarm-Block-Web/src/app/components/preferences/preferences.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-preferences',
+  imports: [RouterLink],
+  templateUrl: './preferences.html',
+  styleUrl: './preferences.css',
+})
+export class Preferences {
+
+}
